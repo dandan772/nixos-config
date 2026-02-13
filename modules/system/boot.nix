216f05979bd0c561ef1bdfc6d1d1f851aaf0cf8e
Dan <andzerotwo@protonmail.com>
2026-02-13 19:35:50 -0300

    bootloader

--- modules/system/boot.nix
+++ modules/system/boot.nix
@@ -3,6 +3,10 @@
 {
   boot.loader.systemd-boot.enable = true;
   boot.loader.efi.canTouchEfiVariables = true;
-
+  boot.loader.timeout = 2;
+  boot.loader.systemd-boot.extraInstallCommands = ''
+      echo "default auto-windows" >> /boot/loader/loader.conf
+  '';
+  
   boot.kernelPackages = pkgs.linuxPackages_6_18;
 }
