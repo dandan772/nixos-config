30dd9903a2208b439a9fce798290036eb77f3502
Dan <andzerotwo@protonmail.com>
2026-02-13 14:50:16 -0300

    enable nix flakes globally

---
+++ modules/system/nvidia.nix
@@ -0,0 +1,19 @@
+{ config, pkgs, ... }:
+
+{
+  services.xserver.videoDrivers = [ "nvidia" ];
+
+  hardware.graphics.enable = true;
+
+  hardware.nvidia = {
+    modesetting.enable = true;
+    powerManagement.enable = true;
+    open = false;
+
+    prime = {
+      offload.enable = true;
+      amdgpuBusId = "PCI:6:0:0";
+      nvidiaBusId = "PCI:1:0:0"; 
+    };
+  };
+}
