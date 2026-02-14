2d598a86b171d3a39b367e462f0b91695a694a65
Dan <andzerotwo@protonmail.com>
2026-02-14 13:53:52 -0300

    libvirt

--- modules/system/virtualization.nix
+++ modules/system/virtualization.nix
@@ -1,16 +1,20 @@
 { config, pkgs, ... }:
 
 {
-  # Habilita libvirtd
+  # Libvirt
   virtualisation.libvirtd.enable = true;
   virtualisation.libvirtd.socketEnable = true;
 
-  # DNS/DHCP se quiser
+  # DNS/DHCP
   services.dnsmasq.enable = true;
 
   # Programas
   environment.systemPackages = with pkgs; [
-    virt-manager
-    qemu
+    virt-manager   # GUI pra gerenciar VMs
+    qemu           # Máquina virtual em si
+    libvirt        # Biblioteca necessária pro virt-manager
   ];
+
+  # Permite que usuários do grupo 'libvirt' usem KVM sem sudo
+  users.groups.libvirt = {};
 }
