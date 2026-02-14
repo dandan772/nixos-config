5d3f1e8097bf837bbe8cb09da4f3ed34dcb28a46
Dan <andzerotwo@protonmail.com>
2026-02-14 13:39:28 -0300

    libvirt

--- modules/system/virtualization.nix
+++ modules/system/virtualization.nix
@@ -1,19 +1,13 @@
 { config, pkgs, ... }:
 
 {
-  virtualisation.libvirtd = {
-    enable = true;
-    qemu = {
-      package = pkgs.qemu_kvm;
-      runAsRoot = true;
-      swtpm.enable = true;
-      # A opção ovmf.enable foi removida aqui, pois agora é padrão.
-    };
-  };
-
-  services.dnsmasq.enable = true;
+  virtualization.libvirtd.enable = true;
+  virtualization.libvirtd.socketEnable = true;
 
   environment.systemPackages = with pkgs; [
     virt-manager
+    qemu
   ];
+
+  services.dnsmasq.enable = true;
 }
