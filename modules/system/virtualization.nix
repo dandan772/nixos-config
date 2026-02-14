41349042f7641e42b5c9bad241f0d6593f9d00b5
Dan <andzerotwo@protonmail.com>
2026-02-14 13:33:20 -0300

    libvirt

--- modules/system/virtualization.nix
+++ modules/system/virtualization.nix
@@ -1,12 +1,20 @@
 { config, pkgs, ... }:
 
 {
-  virtualisation.libvirtd.enable = true;
+  virtualisation.libvirtd = {
+    enable = true;
+    qemu = {
+      package = pkgs.qemu_kvm;
+      runAsRoot = true; # Garante permissões de escrita em dispositivos de bloco
+      swtpm.enable = true; # Útil para Windows 11 (TPM)
+      ovmf.enable = true;  # Habilita suporte a UEFI
+    };
+  };
 
+  # Opcional: Útil para redes de VMs
   services.dnsmasq.enable = true;
 
   environment.systemPackages = with pkgs; [
     virt-manager
-    qemu
   ];
 }
