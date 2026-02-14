5379d27f03a6d4272908c880f0e4434a823718ea
Dan <andzerotwo@protonmail.com>
2026-02-14 13:33:56 -0300

    libvirt

--- modules/system/virtualization.nix
+++ modules/system/virtualization.nix
@@ -5,13 +5,12 @@
     enable = true;
     qemu = {
       package = pkgs.qemu_kvm;
-      runAsRoot = true; # Garante permissões de escrita em dispositivos de bloco
-      swtpm.enable = true; # Útil para Windows 11 (TPM)
-      ovmf.enable = true;  # Habilita suporte a UEFI
+      runAsRoot = true;
+      swtpm.enable = true;
+      # A opção ovmf.enable foi removida aqui, pois agora é padrão.
     };
   };
 
-  # Opcional: Útil para redes de VMs
   services.dnsmasq.enable = true;
 
   environment.systemPackages = with pkgs; [
