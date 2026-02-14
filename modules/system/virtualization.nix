df81e4f65f403700ce372713f1184da0aacbebdb
Dan <andzerotwo@protonmail.com>
2026-02-14 13:29:11 -0300

    libvirt

--- modules/system/virtualization.nix
+++ modules/system/virtualization.nix
@@ -1,17 +1,13 @@
 { config, pkgs, ... }:
 
 {
-  # Ativa o libvirtd no sistema
   virtualisation.libvirtd.enable = true;
+  virtualisation.libvirtd.socketEnable = true;
 
-  # QEMU para VMs
-  virtualisation.qemu.package = pkgs.qemu;
-
-  # DNSMASQ para rede NAT (opcional)
   services.dnsmasq.enable = true;
 
-  # Ferramentas do usuário
   environment.systemPackages = with pkgs; [
     virt-manager
+    qemu
   ];
 }
