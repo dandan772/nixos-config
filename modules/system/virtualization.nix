9ee4b65077e2055118e68353741434efafa1a851
Dan <andzerotwo@protonmail.com>
2026-02-14 14:01:53 -0300

    libvirt

--- modules/system/virtualization.nix
+++ modules/system/virtualization.nix
@@ -1,16 +1,36 @@
 { config, pkgs, ... }:
-
 {
-  # Libvirt
-  virtualisation.libvirtd.enable = true;
+  # Libvirt with QEMU/KVM
+  virtualisation.libvirtd = {
+    enable = true;
+    qemu = {
+      package = pkgs.qemu_kvm;
+      runAsRoot = true;
+      swtpm.enable = true;
+      ovmf = {
+        enable = true;
+        packages = [(pkgs.OVMF.override {
+          secureBoot = true;
+          tpmSupport = true;
+        }).fd];
+      };
+    };
+  };
 
-  # Programas pro sistema
+  # Programs
   environment.systemPackages = with pkgs; [
     virt-manager
-    qemu
-    libvirt
+    virt-viewer
+    spice 
+    spice-gtk
+    spice-protocol
+    win-virtio
+    win-spice
   ];
 
-  # DNS/DHCP opcional
+  # Optional DNS/DHCP
   services.dnsmasq.enable = true;
+  
+  # Enable dconf (required for virt-manager)
+  programs.dconf.enable = true;
 }
