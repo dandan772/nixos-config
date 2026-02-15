86805983d6e4372ac757bff99c1e3a86fe116ff3
Dan <andzerotwo@protonmail.com>
2026-02-15 11:50:45 -0300

    davince

--- flake.nix
+++ flake.nix
@@ -3,6 +3,7 @@
 
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.11";
+
     home-manager = {
       url = "github:nix-community/home-manager/release-25.11";
       inputs.nixpkgs.follows = "nixpkgs";
@@ -12,9 +13,6 @@
   outputs = { self, nixpkgs, home-manager, ... }@inputs:
   let
     system = "x86_64-linux";
-    overlays = [
-      (import ./modules/system/davince.nix)
-    ];
   in {
     nixosConfigurations.nixos = nixpkgs.lib.nixosSystem {
       inherit system;
@@ -23,17 +21,25 @@
 
       modules = [
         ./hosts/nixos/default.nix
+
+        # Home Manager NixOS module
         home-manager.nixosModules.default
+
         {
+          # Configurações do Home Manager
           home-manager.useGlobalPkgs = true;
           home-manager.useUserPackages = true;
-
           home-manager.users.dan =
             import ./modules/home/dan.nix;
         }
-      ];
 
-      nixpkgs.overlays = overlays;
+        {
+          # Overlays do Nixpkgs (Davinci, outros)
+          nixpkgs.overlays = [
+            (import ./modules/system/davince.nix)
+          ];
+        }
+      ];
     };
   };
 }
